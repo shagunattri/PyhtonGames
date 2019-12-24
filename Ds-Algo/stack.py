eb1e339c159df7fa4f7456cc7e088175dab458f2
id10t <shagunattri17@gmail.com>
2019-12-24 23:49:40 +0530

    DS stacks

---
+++ Ds-Algo/stack.py
@@ -0,0 +1,33 @@
+# LIFO
+
+class Stack():
+    def __init__(self):
+        self.items =[]
+    
+    def push(self,item):
+        self.items.append(item)
+        
+    def pop(self):
+        self.items.pop()
+        
+    def is_empty(self):
+        return self.items == []
+    
+    def peek(self):
+        if not self.is_empty():
+            return self.items[-1]
+        
+    def get_stack(self):
+        return self.items
+    
+
+s = Stack()
+s.push('A')
+print('Is Stack Empty: ' + str(s.is_empty()))
+s.push('B')
+s.push('C')
+s.pop()
+s.push('D')
+print('At the Peak: ' + s.peek())
+print('     Stack:      ')
+print(s.get_stack())
