97cc5735a841fb468bf8279bb4cf35d8e7104ba9
shagunattri <shagunattri17@gmail.com>
2020-04-30 22:26:23 +0530

    Day 12 class and static methods

---
+++ python/solves/D12/1_1.py
@@ -0,0 +1,8 @@
+class indian():
+    @staticmethod
+    def printN():
+        print("I am Indian")
+
+india = indian()
+india.printN()
+india.printN()
---
+++ python/solves/D12/1_2.py
@@ -0,0 +1,11 @@
+class Indian():
+    pass
+
+class Delhi(Indian):
+    pass
+
+indian = Indian()
+delhi = Delhi()
+
+print(indian)
+print(delhi)
