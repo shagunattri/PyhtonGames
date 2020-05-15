acef20ad1eba4ec9ddb3c2dac6892c1c8dfa4d72
shagunattri <shagunattri17@gmail.com>
2020-05-15 21:50:34 +0530

    prac sess

---
+++ python/solves/D23/1_1.py
@@ -0,0 +1,7 @@
+import textwrap
+
+str = input()
+
+width = int(input())
+
+print(textwrap.fill(str,width))
---
+++ python/solves/D23/1_2.py
@@ -0,0 +1,7 @@
+import calendar
+
+month,day,year = map(int,input().split())
+
+
+dayId = calendar.weekday(year,month,day)
+print(calendar.day_name[dayId].upper())
